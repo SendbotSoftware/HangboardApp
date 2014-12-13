cc73565f1eeb625d06d7dae0c0b3523c7fdcfe43
Rohan Balakrishnan <rohanbk@gmail.com>
2014-12-13 01:35:21 -0800

    Get some more of the page router stuff to work

---
+++ js/views/EditWorkoutView.js
@@ -0,0 +1,11 @@
+define([
+  'jQuery',
+  'handlebars',
+  'Backbone',
+  'text!templates/EditWorkoutTemplate.html'
+], function($, handlebars, Backbone, editWorkoutTemplate){
+    var EditWorkoutView = Backbone.View.extend({
+
+    });
+  return EditWorkoutView;
+});
