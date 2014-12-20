ceacf0d6df77317a3459a927edf835f6cbb5e801
Rohan Balakrishnan <rohanbk@gmail.com>
2014-12-19 22:55:48 -0800

    Adding express GET calls to mongo via mongoose (still not working for hangboarding collection)

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var express = require('express')
     , application_root = __dirname
     , mongoose = require('mongoose')
     , config = require('./config')
-    , app = express();
+    , app = express()
+    , Schema = mongoose.Schema;
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -21,13 +22,33 @@ app.use('/', routes);
 if (app.get('env') === 'development') {
     console.log("Attempting to connect to dev environment Mongo instance...");
     // TODO figure out how to use config.js
-    mongoose.connect('mongodb://localhost:27017/handboardWorkouts');
+    mongoose.connect('mongodb://localhost/climbTraining');
 }
 
-app.get('/workoutsList', function(req, res) {
-    // TODO connect to mongo via mongoose here
-    res.send('getting workouts');
+// TODO this is a temporary schema just to experiment with Mongoose + mongo
+var hangboardingSchema = new Schema({
+    date: String,
+    index: String,
+    type: String,
+    repetitions: String,
+    weight: Number,
+    RPE: Number
+});
+mongoose.model('hangboarding', hangboardingSchema);
+var userSchema = new Schema({name : String});
+mongoose.model('users', userSchema);
+
+app.get('/hangboarding', function(req, res) {
+    mongoose.model('hangboarding').find(function(err, workouts) {
+        console.log(workouts);
+        res.send(workouts);
+    });
 });
 
+app.get('/users', function(req, res) {
+    mongoose.model('users').find(function(err, foo) {
+        res.send(foo);
+    });
+});
 
 module.exports = app;
